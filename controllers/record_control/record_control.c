7a09bb4303556e0d74cbb2ce75ef368163f6af38
Autism_Huang <609886059@qq.com>
2023-01-05 15:11:17 +0800

    1. 新增位置重置功能 2. 将可修改的用户宏定义全部取消，转换成键控模式切换。 3. 新增读取csv行数的功能，代替宏定义csv数据的行数。 4. 新增在终端的LOG窗口界面。 （用户仅需将csv文件扔在文件夹内就可以运行）

--- controllers/record_control/record_control.c
+++ controllers/record_control/record_control.c
@@ -22,12 +22,10 @@
 /*
  * You may want to add macros here.
  */
-#define TIME_STEP 64
+#define TIME_STEP 32
 
-#define VIEWFOLLOW ON                                       // Follow View can set ON or OFF
-#define CSV_PATH ".\\data.csv" // File Path Set
-#define DATA_NUM 1826                                         // data total num
-#define scale 0.05                                            // plotting scale set
+#define CSV_PATH "..\\..\\data.csv" // File Path Set
+#define scale 0.05                  // plotting scale set
 
 #define ON true
 #define OFF false
@@ -48,8 +46,8 @@ typedef struct
     double angle;
 } AXISANGLE_t;
 
-QUATERNION_t quaternion[DATA_NUM];
-double H[DATA_NUM];
+QUATERNION_t *quaternion;
+double *H;
 AXISANGLE_t axis_angle;
 /*
  * This is the main program.
@@ -58,20 +56,28 @@ AXISANGLE_t axis_angle;
  */
 void quaternion_axis(QUATERNION_t *quaternion); // 将四元数转化为轴角
 void automode(int ms);                          // 自动模式
-void read_csv(void);                            // 读取*.csv文件
-char *get_field(char *line, int num);           // 获取csv文件的数据
+void calcrow_csv(void);
+void OperationShow(void);
+void LOG_HMI(void);
+void read_csv(void);                  // 读取*.csv文件
+char *get_field(char *line, int num); // 获取csv文件的数据
 char *remove_quoted(char *str);
 
-static int index = 0;     // 离散点下标
-static int manual = 0;    // 手动控制标志位
-static int automatic = 0; // 自动标志位
+static int index = 0;       // 离散点下标
+static int manual = 0;      // 手动控制标志位
+static int automatic = 0;   // 自动标志位
+static int vision_mode = 0; // 视角模式选择：0.定视角模式 1.自由角模式
+static int DATA_NUM = 0;    // 数据行数
 
 int main(int argc, char **argv)
 {
     /* necessary to initialize webots stuff */
+    LOG_HMI();
     wb_robot_init();
     // Load *.csv data
+    calcrow_csv();
     read_csv();
+    OperationShow();
     /* World and Object Initialize Start */
     // Get Node's DEF
     WbNodeRef node = wb_supervisor_node_get_from_def("AIR");
@@ -90,21 +96,24 @@ int main(int argc, char **argv)
     wb_supervisor_field_set_sf_vec3f(floor_pos, floor_init);
     wb_supervisor_field_set_sf_vec3f(pos, data_pos);
 
-#if VIEWFOLLOW == ON
+    // Vision-FollowMode Init
     WbFieldRef view_pos = wb_supervisor_node_get_field(view_node, "position");
     WbFieldRef view_rot = wb_supervisor_node_get_field(view_node, "orientation");
     double viewpos_follow[3] = {-2.09, 0.00201, 0.479 + H[0] * scale};
+    double viewrot_follow[4] = {-0.528,0.528,0.664,4.31};
+    
+    // 只有在初始化的时候才需要用到这个定位
+    // const double *Air_Rot = wb_supervisor_field_get_sf_rotation(rot);
+    // double viewrot_follow[4] = {Air_Rot[0] - 0.499, Air_Rot[1] + 0.499, Air_Rot[2] - 0.292, 4.436 + Air_Rot[3]};
+    
     wb_supervisor_field_set_sf_vec3f(view_pos, viewpos_follow);
-
-    const double *Air_Rot = wb_supervisor_field_get_sf_rotation(rot);
-    double viewrot_follow[4] = {Air_Rot[0] - 0.499, Air_Rot[1] + 0.499, Air_Rot[2] - 0.292, 4.436 + Air_Rot[3]};
     wb_supervisor_field_set_sf_rotation(view_rot, viewrot_follow);
-#else
+
+    // Vision-MultiVisionMode Init
     WbFieldRef follow_Type = wb_supervisor_node_get_field(view_node, "followType");
     WbFieldRef follow_Name = wb_supervisor_node_get_field(view_node, "follow");
     wb_supervisor_field_set_sf_string(follow_Name, "Plane");
-    wb_supervisor_field_set_sf_string(follow_Type, "Pan and Tilt Shot");
-#endif
+
     /* World and Object Initialize End */
     int timestep = (int)wb_robot_get_basic_time_step();
     wb_keyboard_enable(timestep);
@@ -141,10 +150,11 @@ int main(int argc, char **argv)
                 data_rot[1] = axis_angle.y;
                 data_rot[2] = axis_angle.z;
                 data_rot[3] = axis_angle.angle;
-#if VIEWFOLLOW == ON
-                viewpos_follow[2] = 0.479 + H[index] * scale;
-                wb_supervisor_field_set_sf_vec3f(view_pos, viewpos_follow);
-#endif
+                if (vision_mode == 0)
+                {
+                    viewpos_follow[2] = 0.479 + H[index] * scale;
+                    wb_supervisor_field_set_sf_vec3f(view_pos, viewpos_follow);
+                }
                 wb_supervisor_field_set_sf_vec3f(pos, data_pos);
                 wb_supervisor_field_set_sf_rotation(rot, data_rot);
                 printf("Altitude Reset\n");
@@ -163,10 +173,11 @@ int main(int argc, char **argv)
                 data_rot[1] = axis_angle.y;
                 data_rot[2] = axis_angle.z;
                 data_rot[3] = axis_angle.angle;
-#if VIEWFOLLOW == ON
-                viewpos_follow[2] = 0.479 + H[index] * scale;
-                wb_supervisor_field_set_sf_vec3f(view_pos, viewpos_follow);
-#endif
+                if (vision_mode == 0)
+                {
+                    viewpos_follow[2] = 0.479 + H[index] * scale;
+                    wb_supervisor_field_set_sf_vec3f(view_pos, viewpos_follow);
+                }
                 wb_supervisor_field_set_sf_vec3f(pos, data_pos);
                 wb_supervisor_field_set_sf_rotation(rot, data_rot);
                 printf("H = %f\n", H[index]);
@@ -185,13 +196,27 @@ int main(int argc, char **argv)
                 data_rot[1] = axis_angle.y;
                 data_rot[2] = axis_angle.z;
                 data_rot[3] = axis_angle.angle;
-#if VIEWFOLLOW == ON
-                viewpos_follow[2] = 0.479 + H[index] * scale;
-                wb_supervisor_field_set_sf_vec3f(view_pos, viewpos_follow);
-#endif
+                if (vision_mode == 0)
+                {
+                    viewpos_follow[2] = 0.479 + H[index] * scale;
+                    wb_supervisor_field_set_sf_vec3f(view_pos, viewpos_follow);
+                }
                 wb_supervisor_field_set_sf_vec3f(pos, data_pos);
                 wb_supervisor_field_set_sf_rotation(rot, data_rot);
                 printf("H = %f\n", H[index]);
+                break;
+            case (WB_KEYBOARD_ALT + WB_KEYBOARD_UP):
+                vision_mode = 1;
+                wb_supervisor_field_set_sf_string(follow_Type, "Pan and Tilt Shot");
+                printf("Mode is change to Multiply View Mode\n");
+                break;
+            case (WB_KEYBOARD_ALT + WB_KEYBOARD_DOWN):
+                vision_mode = 0;
+                wb_supervisor_field_set_sf_rotation(view_rot, viewrot_follow);
+                viewpos_follow[2] = 0.479 + H[index] * scale;
+                wb_supervisor_field_set_sf_vec3f(view_pos, viewpos_follow);
+                wb_supervisor_field_set_sf_string(follow_Type, "None");
+                printf("Mode is change to FixedView Mode\n");
                 break;
             }
             key = wb_keyboard_get_key();
@@ -218,6 +243,15 @@ void read_csv()
     static int id = 0;
     char *tmp;
     printf("LOADING DATA....\n");
+
+    fgets(row, DATA_NUM, fp);
+    char *header_1 = get_field(strdup(row), 1);
+    char *header_2 = get_field(strdup(row), 2);
+    char *header_3 = get_field(strdup(row), 3);
+    char *header_4 = get_field(strdup(row), 4);
+    char *header_5 = get_field(strdup(row), 5);
+    printf("Data import sequence:%s->%s->%s->%s->%s\n", header_1, header_2, header_3, header_4, header_5);
+
     while (fgets(row, DATA_NUM, fp) != NULL)
     {
         tmp = get_field(strdup(row), 1);
@@ -235,10 +269,9 @@ void read_csv()
         tmp = get_field(strdup(row), 5);
         quaternion[id].q4 = atof(tmp);
         id++;
-        // wb_robot_step(2);
         if (id == DATA_NUM)
         {
-            printf("Load Data Complete.Total num is %d\n", id);
+            printf("Load Data Complete.\nTotal num is %d\n", id);
             break;
         }
     }
@@ -297,10 +330,16 @@ void automode(int ms)
     double data_pos[3] = {0, 0, H[0]};
     double data_rot[4] = {0, 1, 0, 0};
 
-#if VIEWFOLLOW == ON
+    // Vision-FollowMode Init
     WbFieldRef view_pos = wb_supervisor_node_get_field(view_node, "position");
+    WbFieldRef view_rot = wb_supervisor_node_get_field(view_node, "orientation");
     double viewpos_follow[3] = {-2.09, 0.00201, 0.479 + H[0] * scale};
-#endif
+    double viewrot_follow[4] = {-0.528,0.528,0.664,4.31};
+
+    // Vision-MultiVisionMode Init
+    WbFieldRef follow_Type = wb_supervisor_node_get_field(view_node, "followType");
+    WbFieldRef follow_Name = wb_supervisor_node_get_field(view_node, "follow");
+    wb_supervisor_field_set_sf_string(follow_Name, "Plane");
 
     while ((wb_robot_step(ms) != -1) && automatic == 1)
     {
@@ -311,10 +350,12 @@ void automode(int ms)
         data_rot[1] = axis_angle.y;
         data_rot[2] = axis_angle.z;
         data_rot[3] = axis_angle.angle;
-#if VIEWFOLLOW == ON
-        viewpos_follow[2] = 0.479 + H[index] * scale;
-        wb_supervisor_field_set_sf_vec3f(view_pos, viewpos_follow);
-#endif
+
+        if (vision_mode == 0)
+        {
+            viewpos_follow[2] = 0.479 + H[index] * scale;
+            wb_supervisor_field_set_sf_vec3f(view_pos, viewpos_follow);
+        }
         wb_supervisor_field_set_sf_vec3f(pos, data_pos);
         wb_supervisor_field_set_sf_rotation(rot, data_rot);
         ++index;
@@ -329,6 +370,19 @@ void automode(int ms)
                 manual = 1;
                 printf("Auto Mode is Pause\n");
                 break;
+            case (WB_KEYBOARD_ALT + WB_KEYBOARD_UP):
+                vision_mode = 1;
+                wb_supervisor_field_set_sf_string(follow_Type, "Pan and Tilt Shot");
+                printf("Mode is change to Multiply View Mode\n");
+                break;
+            case (WB_KEYBOARD_ALT + WB_KEYBOARD_DOWN):
+                vision_mode = 0;
+                wb_supervisor_field_set_sf_rotation(view_rot, viewrot_follow);
+                viewpos_follow[2] = 0.479 + H[index] * scale;
+                wb_supervisor_field_set_sf_vec3f(view_pos, viewpos_follow);
+                wb_supervisor_field_set_sf_string(follow_Type, "None");
+                printf("Mode is change to FixedView Mode\n");
+                break;
             }
             key = wb_keyboard_get_key();
         }
@@ -339,4 +393,56 @@ void automode(int ms)
             break;
         }
     }
+}
+inline void calcrow_csv()
+{
+    FILE *file_csv;
+    file_csv = fopen(CSV_PATH, "r");
+    if (file_csv == NULL)
+    {
+        fprintf(stderr, "fopen() failed.\n");
+        exit(EXIT_FAILURE);
+    }
+    int flag;
+    while (!feof(file_csv))
+    {
+        flag = fgetc(file_csv);
+        if (flag == '\n')
+        {
+            DATA_NUM++;
+        }
+    }
+    fclose(file_csv);
+    DATA_NUM -= 1; // 删除开头行的标识
+    quaternion = (QUATERNION_t *)malloc(sizeof(QUATERNION_t) * DATA_NUM);
+    H = (double *)malloc(sizeof(double) * DATA_NUM);
+    printf("DATA_NUM is :%d\n", DATA_NUM);
+}
+
+void LOG_HMI()
+{
+    printf("/*****************************************************************/\n");
+    printf("/*          _____   _            __   _                          */\n");
+    printf("/*         |  ___| | |  _   _   / _| (_)  _ __    ___            */\n");
+    printf("/*         | |_    | | | | | | | |_  | | | \"__| / _  |           */\n");
+    printf("/*         |  _|   | | | |_| | |  _| | | | |    |  __/           */\n");
+    printf("/*         |_|     |_| |__, |  |_|   |_| |_|    |___|            */\n");
+    printf("/*                      |___/                                    */\n");
+    printf("/*---------------------------------------------------------------*/\n");
+    printf("/*         This file is mainly used for flight data playback,    */\n");
+    printf("/*              please open the Readme for details               */\n");
+    printf("/*                                          Author:Autism_Huang  */\n");
+    printf("/*****************************************************************/\n");
+}
+void OperationShow()
+{
+    printf("/************************************/\n");
+    printf("/*  ctrl+↑：Auto Record *.csv data  */\n");
+    printf("/*  ctrl+↓：Pause auto Record       */\n");
+    printf("/*   alt+↑：Multiple view mode      */\n");
+    printf("/*   alt+↓：Fixed view mode(Default)*/\n");
+    printf("/*     ↓  ：Reset                   */\n");
+    printf("/*     ←  ：Play one frame back     */\n");
+    printf("/*     →  ：Play one frame forward  */\n");
+    printf("/************************************/\n");
 }
