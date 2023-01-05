ea2655d2f2ab951e32a1de17d38a0704a0f6898c
Autism_Huang <609886059@qq.com>
2023-01-05 15:13:06 +0800

    1. 新增仿真高度补偿 2. 新增输出t时间以及轴角度。

--- controllers/record_control/record_control.c
+++ controllers/record_control/record_control.c
@@ -25,10 +25,7 @@
 #define TIME_STEP 32
 
 #define CSV_PATH "..\\..\\data.csv" // File Path Set
-#define scale 0.05                  // plotting scale set
-
-#define ON true
-#define OFF false
+#define scale 1                   // plotting scale set
 
 typedef struct
 {
@@ -46,16 +43,26 @@ typedef struct
     double angle;
 } AXISANGLE_t;
 
+typedef struct
+{
+    float Pitch;
+    float Roll;
+    float Yaw;
+} EULER_t;
+
 QUATERNION_t *quaternion;
 double *H;
+double *t;
 AXISANGLE_t axis_angle;
+EULER_t euler;
 /*
  * This is the main program.
  * The arguments of the main function can be specified by the
  * "controllerArgs" field of the Robot node
  */
 void quaternion_axis(QUATERNION_t *quaternion); // 将四元数转化为轴角
-void automode(int ms);                          // 自动模式
+void quaternion_euler(QUATERNION_t *quaternion);
+void automode(int ms); // 自动模式
 void calcrow_csv(void);
 void OperationShow(void);
 void LOG_HMI(void);
@@ -68,7 +75,7 @@ static int manual = 0;      // 手动控制标志位
 static int automatic = 0;   // 自动标志位
 static int vision_mode = 0; // 视角模式选择：0.定视角模式 1.自由角模式
 static int DATA_NUM = 0;    // 数据行数
-
+static double altimetric_compensation = 0; // 高度补偿
 int main(int argc, char **argv)
 {
     /* necessary to initialize webots stuff */
@@ -88,10 +95,11 @@ int main(int argc, char **argv)
     WbFieldRef floor_pos = wb_supervisor_node_get_field(floor_node, "translation");
     WbFieldRef pos = wb_supervisor_node_get_field(node, "translation");
     WbFieldRef rot = wb_supervisor_node_get_field(node, "rotation");
-
-    double data_pos[3] = {0, 0, H[0] * scale};
+    
+    altimetric_compensation = H[0] < 0 ? abs(H[0] * scale) : (-H[0] * scale);
+    double data_pos[3] = {0, 0, H[0] * scale + altimetric_compensation};
     double data_rot[4] = {0, 1, 0, 0};
-    double floor_init[3] = {0, 0, H[0] * scale - 0.06};
+    double floor_init[3] = {0, 0, H[0] * scale + altimetric_compensation - 0.06};
 
     wb_supervisor_field_set_sf_vec3f(floor_pos, floor_init);
     wb_supervisor_field_set_sf_vec3f(pos, data_pos);
@@ -99,13 +107,13 @@ int main(int argc, char **argv)
     // Vision-FollowMode Init
     WbFieldRef view_pos = wb_supervisor_node_get_field(view_node, "position");
     WbFieldRef view_rot = wb_supervisor_node_get_field(view_node, "orientation");
-    double viewpos_follow[3] = {-2.09, 0.00201, 0.479 + H[0] * scale};
-    double viewrot_follow[4] = {-0.528,0.528,0.664,4.31};
-    
+    double viewpos_follow[3] = {-2.09, 0.00201, 0.479 + H[0] * scale + altimetric_compensation};
+    double viewrot_follow[4] = {-0.528, 0.528, 0.664, 4.31};
+
     // 只有在初始化的时候才需要用到这个定位
     // const double *Air_Rot = wb_supervisor_field_get_sf_rotation(rot);
     // double viewrot_follow[4] = {Air_Rot[0] - 0.499, Air_Rot[1] + 0.499, Air_Rot[2] - 0.292, 4.436 + Air_Rot[3]};
-    
+
     wb_supervisor_field_set_sf_vec3f(view_pos, viewpos_follow);
     wb_supervisor_field_set_sf_rotation(view_rot, viewrot_follow);
 
@@ -144,7 +152,8 @@ int main(int argc, char **argv)
             case WB_KEYBOARD_DOWN:
                 index = 0;
                 quaternion_axis(&quaternion[index]);
-                data_pos[2] = H[index] * scale;
+                H[index] = H[index] > H[0] ? H[index]:H[0];
+                data_pos[2] = H[index] * scale + altimetric_compensation;
 
                 data_rot[0] = axis_angle.x;
                 data_rot[1] = axis_angle.y;
@@ -152,7 +161,7 @@ int main(int argc, char **argv)
                 data_rot[3] = axis_angle.angle;
                 if (vision_mode == 0)
                 {
-                    viewpos_follow[2] = 0.479 + H[index] * scale;
+                    viewpos_follow[2] = 0.479 + H[index] * scale + altimetric_compensation;
                     wb_supervisor_field_set_sf_vec3f(view_pos, viewpos_follow);
                 }
                 wb_supervisor_field_set_sf_vec3f(pos, data_pos);
@@ -167,7 +176,9 @@ int main(int argc, char **argv)
                     printf("index is lowest,do not press left\n");
                 }
                 quaternion_axis(&quaternion[index]);
-                data_pos[2] = H[index] * scale;
+                quaternion_euler(&quaternion[index]);
+                H[index] = H[index] > H[0] ? H[index]:H[0];
+                data_pos[2] = H[index] * scale + altimetric_compensation;
 
                 data_rot[0] = axis_angle.x;
                 data_rot[1] = axis_angle.y;
@@ -175,12 +186,12 @@ int main(int argc, char **argv)
                 data_rot[3] = axis_angle.angle;
                 if (vision_mode == 0)
                 {
-                    viewpos_follow[2] = 0.479 + H[index] * scale;
+                    viewpos_follow[2] = 0.479 + H[index] * scale + altimetric_compensation;
                     wb_supervisor_field_set_sf_vec3f(view_pos, viewpos_follow);
                 }
                 wb_supervisor_field_set_sf_vec3f(pos, data_pos);
                 wb_supervisor_field_set_sf_rotation(rot, data_rot);
-                printf("H = %f\n", H[index]);
+                printf("t=%.3f,H = %f,Pitch = %f,Roll = %f,Yaw = %f\n", t[index], H[index], euler.Pitch, euler.Roll, euler.Yaw);
                 break;
             case WB_KEYBOARD_RIGHT:
                 ++index;
@@ -190,7 +201,9 @@ int main(int argc, char **argv)
                     printf("index is highest,do not press right\n");
                 }
                 quaternion_axis(&quaternion[index]);
-                data_pos[2] = H[index] * scale;
+                quaternion_euler(&quaternion[index]);
+                H[index] = H[index] > H[0] ? H[index]:H[0];
+                data_pos[2] = H[index] * scale + altimetric_compensation;
 
                 data_rot[0] = axis_angle.x;
                 data_rot[1] = axis_angle.y;
@@ -198,12 +211,12 @@ int main(int argc, char **argv)
                 data_rot[3] = axis_angle.angle;
                 if (vision_mode == 0)
                 {
-                    viewpos_follow[2] = 0.479 + H[index] * scale;
+                    viewpos_follow[2] = 0.479 + H[index] * scale + altimetric_compensation;
                     wb_supervisor_field_set_sf_vec3f(view_pos, viewpos_follow);
                 }
                 wb_supervisor_field_set_sf_vec3f(pos, data_pos);
                 wb_supervisor_field_set_sf_rotation(rot, data_rot);
-                printf("H = %f\n", H[index]);
+                printf("t=%.3f,H = %f,Pitch = %f,Roll = %f,Yaw = %f\n", t[index], H[index], euler.Pitch, euler.Roll, euler.Yaw);
                 break;
             case (WB_KEYBOARD_ALT + WB_KEYBOARD_UP):
                 vision_mode = 1;
@@ -213,7 +226,8 @@ int main(int argc, char **argv)
             case (WB_KEYBOARD_ALT + WB_KEYBOARD_DOWN):
                 vision_mode = 0;
                 wb_supervisor_field_set_sf_rotation(view_rot, viewrot_follow);
-                viewpos_follow[2] = 0.479 + H[index] * scale;
+                H[index] = H[index] > H[0] ? H[index]:H[0];
+                viewpos_follow[2] = 0.479 + H[index] * scale + altimetric_compensation;
                 wb_supervisor_field_set_sf_vec3f(view_pos, viewpos_follow);
                 wb_supervisor_field_set_sf_string(follow_Type, "None");
                 printf("Mode is change to FixedView Mode\n");
@@ -250,23 +264,27 @@ void read_csv()
     char *header_3 = get_field(strdup(row), 3);
     char *header_4 = get_field(strdup(row), 4);
     char *header_5 = get_field(strdup(row), 5);
-    printf("Data import sequence:%s->%s->%s->%s->%s\n", header_1, header_2, header_3, header_4, header_5);
+    char *header_6 = get_field(strdup(row), 6);
+    printf("Data import sequence:%s->%s->%s->%s->%s->%s\n", header_1, header_2, header_3, header_4, header_5, header_6);
 
     while (fgets(row, DATA_NUM, fp) != NULL)
     {
         tmp = get_field(strdup(row), 1);
-        H[id] = atof(tmp);
+        t[id] = atof(tmp) / 1000;
 
         tmp = get_field(strdup(row), 2);
-        quaternion[id].q1 = atof(tmp);
+        H[id] = atof(tmp);
 
         tmp = get_field(strdup(row), 3);
-        quaternion[id].q2 = atof(tmp);
+        quaternion[id].q1 = atof(tmp);
 
         tmp = get_field(strdup(row), 4);
-        quaternion[id].q3 = atof(tmp);
+        quaternion[id].q2 = atof(tmp);
 
         tmp = get_field(strdup(row), 5);
+        quaternion[id].q3 = atof(tmp);
+
+        tmp = get_field(strdup(row), 6);
         quaternion[id].q4 = atof(tmp);
         id++;
         if (id == DATA_NUM)
@@ -319,6 +337,18 @@ void quaternion_axis(QUATERNION_t *quaternion)
     axis_angle.angle = 2 * acos(qw);
 }
 
+void quaternion_euler(QUATERNION_t *quaternion)
+{
+    double q0 = quaternion->q1;
+    double q1 = quaternion->q2;
+    double q2 = quaternion->q3;
+    double q3 = quaternion->q4;
+    float degrees = (180.0f/(float)M_PI);
+    euler.Pitch = (float)(asin(-2 * q1 * q3 + 2 * q0 * q2) * degrees);
+    euler.Roll = (float)(atan2(2 * q2 * q3 + 2 * q0 * q1, -2 * q1 * q1 - 2 * q2 * q2 + 1) * degrees);
+    euler.Yaw = (float)(atan2(2 * (q1 * q2 + q0 * q3), q0 * q0 + q1 * q1 - q2 * q2 - q3 * q3) * degrees);
+}
+
 void automode(int ms)
 {
     WbNodeRef node = wb_supervisor_node_get_from_def("AIR");
@@ -326,15 +356,16 @@ void automode(int ms)
 
     WbFieldRef pos = wb_supervisor_node_get_field(node, "translation");
     WbFieldRef rot = wb_supervisor_node_get_field(node, "rotation");
-
-    double data_pos[3] = {0, 0, H[0]};
+    
+    H[index] = H[index] > H[0] ? H[index]:H[0];
+    double data_pos[3] = {0, 0, H[0] * scale + altimetric_compensation};
     double data_rot[4] = {0, 1, 0, 0};
 
     // Vision-FollowMode Init
     WbFieldRef view_pos = wb_supervisor_node_get_field(view_node, "position");
     WbFieldRef view_rot = wb_supervisor_node_get_field(view_node, "orientation");
-    double viewpos_follow[3] = {-2.09, 0.00201, 0.479 + H[0] * scale};
-    double viewrot_follow[4] = {-0.528,0.528,0.664,4.31};
+    double viewpos_follow[3] = {-2.09, 0.00201, 0.479 + H[0] * scale + altimetric_compensation};
+    double viewrot_follow[4] = {-0.528, 0.528, 0.664, 4.31};
 
     // Vision-MultiVisionMode Init
     WbFieldRef follow_Type = wb_supervisor_node_get_field(view_node, "followType");
@@ -344,8 +375,9 @@ void automode(int ms)
     while ((wb_robot_step(ms) != -1) && automatic == 1)
     {
         quaternion_axis(&quaternion[index]);
-        data_pos[2] = H[index] * scale;
-
+        quaternion_euler(&quaternion[index]);
+        H[index] = H[index] > H[0] ? H[index]:H[0];
+        data_pos[2] = H[index] * scale + altimetric_compensation;
         data_rot[0] = axis_angle.x;
         data_rot[1] = axis_angle.y;
         data_rot[2] = axis_angle.z;
@@ -353,13 +385,13 @@ void automode(int ms)
 
         if (vision_mode == 0)
         {
-            viewpos_follow[2] = 0.479 + H[index] * scale;
+            viewpos_follow[2] = 0.479 + H[index] * scale + altimetric_compensation;
             wb_supervisor_field_set_sf_vec3f(view_pos, viewpos_follow);
         }
         wb_supervisor_field_set_sf_vec3f(pos, data_pos);
         wb_supervisor_field_set_sf_rotation(rot, data_rot);
+        printf("t=%.3f,H = %f,Pitch = %f,Roll = %f,Yaw = %f\n", t[index], H[index], euler.Pitch, euler.Roll, euler.Yaw);
         ++index;
-        printf("H = %f\n", H[index]);
         int key = wb_keyboard_get_key();
         while (key > 0)
         {
@@ -378,7 +410,7 @@ void automode(int ms)
             case (WB_KEYBOARD_ALT + WB_KEYBOARD_DOWN):
                 vision_mode = 0;
                 wb_supervisor_field_set_sf_rotation(view_rot, viewrot_follow);
-                viewpos_follow[2] = 0.479 + H[index] * scale;
+                viewpos_follow[2] = 0.479 + H[index] * scale + altimetric_compensation;
                 wb_supervisor_field_set_sf_vec3f(view_pos, viewpos_follow);
                 wb_supervisor_field_set_sf_string(follow_Type, "None");
                 printf("Mode is change to FixedView Mode\n");
@@ -416,6 +448,7 @@ inline void calcrow_csv()
     DATA_NUM -= 1; // 删除开头行的标识
     quaternion = (QUATERNION_t *)malloc(sizeof(QUATERNION_t) * DATA_NUM);
     H = (double *)malloc(sizeof(double) * DATA_NUM);
+    t = (double *)malloc(sizeof(double) * DATA_NUM);
     printf("DATA_NUM is :%d\n", DATA_NUM);
 }
 
